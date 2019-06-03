9595e5b48013bf3ff8e660143ddb72354e9c54b4
mschumak <ma.schumaker@gmail.com>
2019-06-03 17:23:17 -0400

    Add MIT license terms to all files

--- ExportTransformedROI.cpp
+++ ExportTransformedROI.cpp
@@ -1,20 +1,39 @@
-/*********************************************************************************
-**********************************************************************************
-		
-		ExportTransformedROI is a plugin for PathCore Sedeen Viewer. 
-		
-		This plugin has been developed by Rushin Shojaii 
-		in Dr. Anne Martel lab at Sunybrook Research Institute.
-		
-		This is a part of pathology image informatics platform (PIIP) 
-		funded by NIH.
-		
-		Contact info: rushin.shojaii@sri.utoronto.ca
+/*=============================================================================
+ *
+ *  Copyright (c) 2019 Sunnybrook Research Institute
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ *
+ *  ExportTransformedROI is a plugin for PathCore Sedeen Viewer.
+ *
+ *  This plugin has been developed by Rushin Shojaii
+ *  in Dr. Anne Martel lab at Sunybrook Research Institute.
+ *
+ *  This is a part of pathology image informatics platform (PIIP)
+ *  funded by NIH.
+ *
+ *  Contact info: rushin.shojaii@sri.utoronto.ca
+ *
+ *  Date of the release of the code: April 26, 2017
+ *
+ *=============================================================================*/
 
-		Date of the release of the code: April 26, 2017
-
-**********************************************************************************
-**********************************************************************************/
 
 // Primary header
 #include "./ExportTransformedROI.h"
